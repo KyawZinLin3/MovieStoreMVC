6a2ca59c4456c84cd3529991cbd9fabf7e679589
KyawZinLin3 <kyawzinlin14future@gmail.com>
2023-11-18 22:28:55 +0630

    Add project files.

---
+++ MovieStoreMVC/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieStoreMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
