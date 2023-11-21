b20265f971bafe0adacdf2ef4c923336f3739252
KyawZinLin3 <kyawzinlin14future@gmail.com>
2023-11-22 00:14:12 +0630

    makin login

--- MovieStoreMVC/Controllers/UserAuthenticationController.cs
+++ MovieStoreMVC/Controllers/UserAuthenticationController.cs
@@ -13,20 +13,45 @@ namespace MovieStoreMVC.Controllers
         }
         //We will create a user with admin rights.after that we are going to comment this method 
         //because we need onlyone user in this application
-        public async Task<IActionResult>  Register()
-        {
-            var model = new RegistrationModel
-            {
-                Email = "admin@gmail.com",
-                Username = "admin",
-                Name = "Kyaw",
-                Password = "Admin@123",
-                PasswordConfirm = "Admin@123",
-                Role = "Admin"
-            };
-            var result = await authService.RegisterAsync(model);
+        //public async Task<IActionResult>  Register()
+        //{
+        //    var model = new RegistrationModel
+        //    {
+        //        Email = "admin@gmail.com",
+        //        Username = "admin",
+        //        Name = "Kyaw",
+        //        Password = "Admin@123",
+        //        PasswordConfirm = "Admin@123",
+        //        Role = "Admin"
+        //    };
+        //    var result = await authService.RegisterAsync(model);
            
-            return Ok(result.Message);
+        //    return Ok(result.Message);
+        //}
+
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                return View(model);
+
+            }
+            var result = await authService.LoginAsync(model);
+            if(result.StatusCode == 1)
+            {
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                TempData["msg"] = "Could not Login....";
+                return RedirectToAction(nameof(Login));
+            }
         }
     }
 }
