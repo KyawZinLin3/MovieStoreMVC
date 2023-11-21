3c233f67e4856f234fb0e28a90b57c76c0d8f67d
KyawZinLin3 <kyawzinlin14future@gmail.com>
2023-11-21 12:33:05 +0630

    making authentication

---
+++ MovieStoreMVC/Models/DTO/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieStoreMVC.Models.DTO
+{
+    public class LoginModel
+    {
+       
+        public string? Username { get; set; }
+        [Required]
+        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*[#$^+=!*()@%&]).{6,}$", ErrorMessage = "Minimum length 6 and must contain  1 Uppercase,1 lowercase, 1 special character and 1 digit")]
+        public string Password { get; set; }
+       
+
+    }
+}
---
+++ MovieStoreMVC/Repositories/Abstract/IUserAuthenticationServices.cs
@@ -0,0 +1,17 @@
+﻿using MovieStoreMVC.Models.DTO;
+
+namespace MovieStoreMVC.Repositories.Abstract
+{
+    public interface IUserAuthenticationServices
+    {
+        Task<Status> LoginAsync(LoginModel model);
+
+        Task LogoutAsync();
+
+        Task<Status> RegisterAsync(RegistrationModel model);
+
+        
+
+
+    }
+}
