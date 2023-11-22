4e42b7809d2229fe119b3cdd9a750a42af2a92e1
KyawZinLin3 <kyawzinlin14future@gmail.com>
2023-11-22 11:24:29 +0630

    Login Complete

--- MovieStoreMVC/Controllers/UserAuthenticationController.cs
+++ MovieStoreMVC/Controllers/UserAuthenticationController.cs
@@ -25,7 +25,7 @@ namespace MovieStoreMVC.Controllers
         //        Role = "Admin"
         //    };
         //    var result = await authService.RegisterAsync(model);
-           
+
         //    return Ok(result.Message);
         //}
 
@@ -37,21 +37,24 @@ namespace MovieStoreMVC.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(LoginModel model)
         {
-            if(ModelState.IsValid)
-            {
+            if (!ModelState.IsValid)
                 return View(model);
 
-            }
             var result = await authService.LoginAsync(model);
-            if(result.StatusCode == 1)
-            {
-                return RedirectToAction("Index","Home");
-            }
+            if (result.StatusCode == 1)
+                return RedirectToAction("Index", "Home");
             else
             {
-                TempData["msg"] = "Could not Login....";
+                TempData["msg"] = "Could not logged in..";
                 return RedirectToAction(nameof(Login));
             }
         }
+
+        public async Task<IActionResult> Logout()
+        {
+            await authService.LogoutAsync();
+            return RedirectToAction(nameof(Login));
+        }
+
     }
 }
